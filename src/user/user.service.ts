23f9af696315bcf44a3bb624dbd4209066af5e38
mmdev110 <mmdev8787@gmail.com>
2024-05-14 11:51:14 +0000

    simple api for practice

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  async createUser(nickname: string): Promise<User> {
+    const exists = await this.repo.existsBy({ nickname: nickname });
+    if (exists) {
+      throw new Error('user already exists');
+    }
+    const _user = this.repo.create({ nickname: nickname });
+    const user = await this.repo.save(_user);
+    return user;
+  }
+  async users(): Promise<User[]> {
+    const users = this.repo.find();
+    return users;
+  }
+}
