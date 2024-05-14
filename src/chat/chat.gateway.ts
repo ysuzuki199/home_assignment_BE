d3d0cc166f5e646dd07f102a1e669878fb9cf146
ysuzuki199 <mmdev878.7@gmail.com>
2024-05-15 08:47:23 +0900

    emit message to client

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,33 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  ConnectedSocket,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleMessage(
+    @MessageBody() payload: string,
+    @ConnectedSocket() client: Socket,
+  ): string {
+    console.log('received');
+    client.emit('message', `Hello world!:  ${payload}`);
+    return `Hello world!:  ${payload}`;
+  }
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log('@@handleConnection: ', client.id);
+  }
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    console.log('@@handleDisconnect: ', client.id);
   }
 }
