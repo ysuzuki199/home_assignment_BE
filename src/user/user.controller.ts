29e4b9abdd7ceb3978353eecc42aecb2962cefe6
ysuzuki199 <mmdev878.7@gmail.com>
2024-05-14 11:51:14 +0000

    simple api for practice

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create_user.dto';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userSearvice: UserService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<User> {
+    const user = await this.userSearvice.createUser(dto.nickname);
+    return user;
+  }
+  @Get()
+  async users(): Promise<User[]> {
+    const users = await this.userSearvice.users();
+    return users;
+  }
+}
