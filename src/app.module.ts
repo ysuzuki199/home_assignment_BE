cc4cadbd1874aa902b034dec176841e6db1bee73
ysuzuki199 <mmdev878.7@gmail.com>
2024-05-14 10:08:29 +0000

    add user module and exec synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['**/*.entity.js'],
+      synchronize: true, //migrate automatically, not recommended in production/staging environment
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
