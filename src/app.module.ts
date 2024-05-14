329e69482e8d17848e03b95cbc7f32db6aeffa80
mmdev110 <mmdev8787@gmail.com>
2024-05-14 21:37:25 +0900

    generate chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: ['**/*.entity.js'],
       synchronize: true, //migrate automatically, not recommended in production/staging environment
     }),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
